00a0736422061e59c96be3604aa6ebc16afaf3b2
elhassane gargem <elhassane.gargem@sciencespo.fr>
2021-10-19 15:09:06 +0200

    skills serializers

--- server/core/models.py
+++ server/core/models.py
@@ -16,6 +16,13 @@ CONTRACT_TYPE = (
 )
 
 
+class Skill(models.Model):
+    name = models.CharField(max_length=30, null=False)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Job(models.Model):
     posted_by = models.ForeignKey(User, related_name='posted_by', on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=100)
@@ -23,6 +30,8 @@ class Job(models.Model):
     salary = models.DecimalField(max_digits=6, decimal_places=2, help_text="Annual Salary", null=True)
     experience = models.IntegerField(default=0, choices=EXPERIENCE_CHOICE, null=True)
     type = models.IntegerField(default=0, choices=CONTRACT_TYPE, null=True)
+
+    skills = models.ManyToManyField('Skill', related_name='jobs')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- server/core/serializers.py
+++ server/core/serializers.py
@@ -1,9 +1,33 @@
 from rest_framework import serializers
-from .models import Job
+from .models import Job, Skill
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
 
 
 class JobSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True)
+
     class Meta:
         model = Job
-        fields = '__all__'
-        read_only_fields = ('id', 'created_at', 'updated_at', 'slug')
+        fields = 'id', 'title', 'description', 'skills'
+        read_only_fields = ('id', 'created_at', 'updated_at', 'slug',)
+
+    def create(self, validated_data):
+        skills_data = validated_data.pop('skills')
+        job = Job.objects.create(**validated_data)
+        for skill in skills_data:
+            obj, created = Skill.objects.get_or_create(**skill)
+
+            job.skills.add(obj)
+        return job
+
+    def update(self, instance, validated_data):
+        skills_data = validated_data.pop('skills')
+        for skill in skills_data:
+            obj, created = Skill.objects.get_or_create(**skill)
+            instance.skills.add(obj)
+        return super().update(instance, validated_data)
