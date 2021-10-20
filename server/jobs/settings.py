10cb9fdb9cbb174af50aec5011a5d99a0a00fe38
elhassane gargem <elhassane.gargem@sciencespo.fr>
2021-10-20 13:35:42 +0200

    cors

--- server/jobs/settings.py
+++ server/jobs/settings.py
@@ -44,9 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -59,6 +59,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
 ]
+CORS_ORIGIN_WHITELIST = CORS_ALLOWED_ORIGINS
 
 ROOT_URLCONF = 'jobs.urls'
 
