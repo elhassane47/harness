883c7d0941685ae57d80f1afb4825f7731b61719
elhassane gargem <elhassane.gargem@sciencespo.fr>
2021-10-24 20:09:08 +0200

    update skills

--- server/core/serializers.py
+++ server/core/serializers.py
@@ -3,9 +3,11 @@ from .models import Job, Skill
 
 
 class SkillSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+
     class Meta:
         model = Skill
-        fields = '__all__'
+        fields = ('id', 'name')
 
 
 class JobSerializer(serializers.ModelSerializer):
@@ -28,6 +30,14 @@ class JobSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         skills_data = validated_data.pop('skills')
         for skill in skills_data:
-            obj, created = Skill.objects.get_or_create(**skill)
-            instance.skills.add(obj)
+            sk_id = skill.get('id', None)
+            sk_name = skill.get('name', None)
+            if sk_id:
+                sk = Skill.objects.get(pk=sk_id)
+                sk.name = sk_name
+                sk.save()
+            else:
+                sk = Skill.objects.create(name=sk_name)
+
+            instance.skills.add(sk)
         return super().update(instance, validated_data)
